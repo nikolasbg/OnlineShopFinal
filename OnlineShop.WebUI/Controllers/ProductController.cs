a4513f4b6b9c939854bcfd045040145e6284eb06
nikolasbg <urosivanovic@gmail.com>
2019-05-30 00:56:39 +0200

    Dodate kategorije i uspostavljeno razvrstavanje artikala uz pomoc istih.

--- OnlineShop.WebUI/Controllers/ProductController.cs
+++ OnlineShop.WebUI/Controllers/ProductController.cs
@@ -4,19 +4,47 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using OnlineShop.Domain.Abstract;
+using OnlineShop.WebUI.Models;
 
 namespace OnlineShop.WebUI.Controllers
 {
     public class ProductController : Controller  //Product kontroler koji je nasledjen iz klase Controller
     {
         private readonly IProductRepository repository;
+        public int PageSize = 4; // postavljamo da cemo imati 4 artikla po strani
+
         public ProductController(IProductRepository repo)
         {
             repository = repo;
         }
-        public ViewResult List()
+        public ViewResult List(string category,int page = 1)
         {
-            return View(repository.Products);
+            ProductsListViewModel model = new ProductsListViewModel
+            {
+
+
+                Products = repository.Products //postavljamo da se po difoltu krece od 1 strane i onda ne oduzimamo nista posto bi onda znacilo da smo na nultoj strani bez artikala...
+                    .Where(p => category == null || p.Category == category)
+                    .OrderBy(p => p.ProductId)
+                    .Skip((page - 1) * PageSize)
+                    .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = category == null ? //prebrojava broj artikala u kategoriji i uklanja visak stranica ako se uslov ispunjava da ima odgovarajuci broj artikala po stranici 
+                        repository.Products.Count() :
+                        repository.Products.Where(p => p.Category == category).Count()
+
+                },
+                CurrentCategory = category
+                
+            };
+
+            return View(model);
+
+
+
         }
     }
 }
---
+++ OnlineShop.WebUI/Models/ProductsListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using OnlineShop.Domain.Entities;
+
+namespace OnlineShop.WebUI.Models
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+        public string CurrentCategory { get; set; }
+    }
+}
